1f17ddbc622083e9b00cdf95ba7ca90108326c16
최유정 <choiyujeong0411@gmail.com>
2025-05-12 10:07:44 +0900

    [OUTPUT] Fix: 비교차트/일반차트에서 임계선 노출

--- app/views/components/visualization/visualization_updater.py
+++ app/views/components/visualization/visualization_updater.py
@@ -59,8 +59,13 @@ class VisualizationUpdater:
                 'value_fontsize': 14,
                 'show_legend': is_comparison,
                 'ylim': None,
-                'thresholds': thresholds,
-                'show_thresholds': True
+                'show_thresholds': True,
+                'thresholds': {
+                    'I': {'lower_limit': 69.5, 'upper_limit': 100},
+                    'D': {'lower_limit': 8.3, 'upper_limit': 100},
+                    'K': {'lower_limit': 8.3, 'upper_limit': 100},
+                    'M': {'lower_limit': 12.5, 'upper_limit': 100}
+                }
             }
         }
      
@@ -83,7 +88,7 @@ class VisualizationUpdater:
                 'ylim': (0, 110), 
                 'threshold_values': [80, 100, 105],
                 'threshold_colors': ['#4CAF50', '#FFC107', '#F44336'],
-                'threshold_labels': ['Good', 'Warning', 'High'] if is_comparison else ['', '', ''],
+                'threshold_labels': ['Good', 'Warning', 'High'],
                 'show_value': True,
                 'value_fontsize': 14,
                 'show_legend': is_comparison
