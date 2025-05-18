56636ee8702802b0918d2708b4ded070a16b929f
이국건 <lkukun@naver.com>
2025-05-18 19:34:32 +0900

    [INPUT] fix: 타입 경고 해결

--- app/core/input/maintenance.py
+++ app/core/input/maintenance.py
@@ -157,8 +157,8 @@ def calc_plan_retention():
     df_result['Next item MFG'] = 0
     for idx,row in df_result.iterrows():
         max_mfg = min(row['Qty'],df_demand_item_mfg[row['Item']])
-        df_result.loc[idx,'Next item MFG'] = max_mfg
-        df_demand_item_mfg[row['Item']] -= max_mfg
+        df_result.loc[idx,'Next item MFG'] = int(max_mfg)
+        df_demand_item_mfg[row['Item']] -= int(max_mfg)
 
     sum_item_qty = df_result['Next item MFG'].sum()
     item_plan_retention = sum_item_qty/sum_qty
@@ -169,8 +169,8 @@ def calc_plan_retention():
     for idx,row in df_result.iterrows():
         rmc = row['Item'][3:11]
         max_mfg = min(row['Qty'],df_demand_rmc_mfg[rmc])
-        df_result.loc[idx,'Next RMC MFG'] = max_mfg
-        df_demand_rmc_mfg[rmc] -= max_mfg
+        df_result.loc[idx,'Next RMC MFG'] = int(max_mfg)
+        df_demand_rmc_mfg[rmc] -= int(max_mfg)
 
     sum_rmc_qty = df_result['Next RMC MFG'].sum()
     rmc_plan_retention = sum_rmc_qty/sum_qty
